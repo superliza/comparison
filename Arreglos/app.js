e48cc7bbdf5eb67279c3ab3704f21a8e512b9d78
superliza <eli_darko@hotmail.com>
2017-10-23 15:14:59 +0000

    trabajando con arreglos

---
+++ Arreglos/app.js
@@ -0,0 +1,8 @@
+var fila = [1,2,3,4]
+var objeto = {perrito: "perrito"};
+fila.shift();
+document.write(fila);
+fila.push("hola");
+document.write("<br>"+fila);
+fila.pop();
+document.write("<br>"+fila);
