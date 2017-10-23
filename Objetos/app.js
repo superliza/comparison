3001f273c8dbc20cc684b307d4a46c24b649f7ca
superliza <eli_darko@hotmail.com>
2017-10-23 16:01:28 +0000

    nuevos docs

---
+++ Objetos/app.js
@@ -0,0 +1,16 @@
+var lasLeias = {
+  name: "Itzel",
+  caracteristicas: {lentes: "rectangulares",
+                    colorChaleco: "vino",
+                    edad: "42"
+                  },
+  frutasPref: ["Manzana", "Jicama", "Piña"],
+
+  saludar: function(){
+    alert("k onda, k pex ");
+  }
+
+}
+
+
+console.log(lasLeias);
